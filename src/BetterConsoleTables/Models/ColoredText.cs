684c75273a5043b66d042ab5c339f85a2aa9f5b9
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-03-26 11:08:56 -0700

    Adds initial colored text model

---
+++ src/BetterConsoleTables/Models/ColoredText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterConsoleTables.Models
+{
+    public class ColoredText
+    {
+        public ColoredText(string value, ConsoleColor color)
+        {
+            Value = value;
+            Color = color;
+        }
+
+        public string Value { get; set; }
+        public ConsoleColor Color { get; set; }
+    }
+}
