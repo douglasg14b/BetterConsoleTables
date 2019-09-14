cd1f22a2718deab4e58a4d4dba2ea54dee348638
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-14 14:04:44 -0700

    Adds ColumnHeader and Enums

---
+++ src/BetterConsoleTables/ColumnHeader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables
+{
+    public struct ColumnHeader
+    {
+        public ColumnHeader(string name, ColumnAlignment alignment = ColumnAlignment.Left)
+        {
+            Title = name;
+            Alignment = alignment;
+        }
+
+        public ColumnHeader(object name, ColumnAlignment alignment = ColumnAlignment.Left)
+            :this(name.ToString(), alignment){ }
+
+        public string Title { get; }
+        public ColumnAlignment Alignment { get; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public static implicit operator ColumnHeader(string value) => new ColumnHeader(value);
+    }
+}
---
+++ src/BetterConsoleTables/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables
+{
+    public enum ColumnAlignment
+    {
+        Left = 0,
+        Right = 1,
+        Center = 2
+    }
+}
