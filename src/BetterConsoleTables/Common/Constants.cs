81881d95afa0a7dbb57bcb6e654ad757bb5f693a
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-15 14:28:48 -0700

    Adds string formatting constants to Constants

--- src/BetterConsoleTables/Common/Constants.cs
+++ src/BetterConsoleTables/Common/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace BetterConsoleTables
 {
     public static class Constants
     {
-        public const ConsoleColor DefaultColor = ConsoleColor.Gray;
+        public static readonly Color DefaultForegoundColor = Color.LightGray;
+        public static readonly Color DefaultBackgroundColor = Color.Black;
+
         public const Alignment DefaultAlignment = Alignment.Left;
+        public const ColorPlane DefaultColorPlane = ColorPlane.Foreground;
+
+        public const string AnsiEsc = "\u001b";
+        public static readonly string DefaultColorCode = AnsiEsc + "[0m";
+
+        /// <summary>
+        /// {0): Plane
+        /// {1}: R
+        /// {2}: G
+        /// {3}: B
+        /// </summary>
+        public static readonly string ColorFormat = AnsiEsc + "[{0};2;{1};{2};{3}m";
+
+        /// <summary>
+        /// {0): Plane
+        /// {1}: R
+        /// {2}: G
+        /// {3}: B
+        /// {4}: String Value
+        /// </summary>
+        public static readonly string FullColorFormat = ColorFormat + "{4}" + DefaultColorCode;
+
+        /// <summary>
+        /// {0): Plane
+        /// {1}: Color Number
+        /// </summary>
+        public static readonly string SimpleColorFormat = AnsiEsc + "[{0};5;{1}m";
+
+        /// <summary>
+        /// {0): Plane
+        /// {1}: Color Number
+        /// {3}: String Value
+        /// </summary>
+        public static readonly string SimpleFullColorFormat = SimpleColorFormat + "{2}" + DefaultColorCode;
+        
 
     }
 }
