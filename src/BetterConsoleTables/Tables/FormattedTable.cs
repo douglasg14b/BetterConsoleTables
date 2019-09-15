975859fa9041c79c32f21182ef5fa3b5503e5174
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-15 00:43:21 -0700

    Adds scaffolded FormattedTable

---
+++ src/BetterConsoleTables/Tables/FormattedTable.cs
@@ -0,0 +1,32 @@
+﻿using BetterConsoleTables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables
+{
+    public class FormattedTable : TableBase<FormattedTable, TableCell, TableCell>
+    {
+        public override FormattedTable AddColumn(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override FormattedTable AddRow(params object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override FormattedTable AddRows(IEnumerable<object[]> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString(int[] columnWidths)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
