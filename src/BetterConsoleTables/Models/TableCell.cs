a38ec82b33f4fad486d2b0302a803ddfb2de86f3
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-14 23:28:15 -0700

    Adds TableCell

---
+++ src/BetterConsoleTables/Models/TableCell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables.Models
+{
+    public class TableCell<TValue>
+    {
+        public TableCell(TValue value, TableCellConfig config)
+        {
+            Value = value.ToString();
+            Color = config.Color;
+            Alignment = config.Alignment;
+        }
+
+        public TableCell(TValue value) :this(value, new TableCellConfig()) { }
+
+        public string Value { get; set; }
+        public ConsoleColor Color { get; set; }
+        public Alignment Alignment { get; set; }
+
+        public int Rows { get; } //Will fill out later
+    }
+
+    public class TableCell : TableCell<string>
+    {
+        public TableCell(string value, TableCellConfig config)
+            :base(value, config) {}
+
+        public TableCell(string value)
+            : base(value, new TableCellConfig()) { }
+    }
+}
