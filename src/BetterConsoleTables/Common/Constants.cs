0a691be9e726e6a04fca5f3f23001fddb15c7827
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-14 23:27:55 -0700

    Adds Constants

---
+++ src/BetterConsoleTables/Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables
+{
+    public static class Constants
+    {
+        public const ConsoleColor DefaultColor = ConsoleColor.Gray;
+        public const Alignment DefaultAlignment = Alignment.Left;
+
+    }
+}
