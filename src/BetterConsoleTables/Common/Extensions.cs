9fd216cb1b96aa852283c03957d52801c20d2eb4
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-15 14:29:47 -0700

    Adds string coloring extensions

--- src/BetterConsoleTables/Common/Extensions.cs
+++ src/BetterConsoleTables/Common/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,20 @@ namespace BetterConsoleTables
 {
     public static class Extensions
     {
+        public static string WithColor(this string value, Color color, ColorPlane plane)
+        {
+            string output = string.Format(Constants.FullColorFormat, (int)plane, color.R, color.G, color.B, value);
+            return output;
+        }
 
+        public static string WithForegroundColor(this string value, Color color)
+        {
+            return value.WithColor(color, ColorPlane.Foreground);
+        }
+
+        public static string WithBackgroundColor(this string value, Color color)
+        {
+            return value.WithColor(color, ColorPlane.Background);
+        }
     }
 }
