44ae20ba04192007587a8d60b8d0c0586e233676
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-14 23:28:07 -0700

    Adds TableCellConfig

---
+++ src/BetterConsoleTables/TableCellConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables
+{
+    public class TableCellConfig
+    {
+        public TableCellConfig(ConsoleColor color = Constants.DefaultColor, Alignment alignment = Constants.DefaultAlignment)
+        {
+            Color = color;
+            Alignment = alignment;
+        }
+
+        public ConsoleColor Color { get; set; }
+        public Alignment Alignment { get; set; }
+    }
+}
