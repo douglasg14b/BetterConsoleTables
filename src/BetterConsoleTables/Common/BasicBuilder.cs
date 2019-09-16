c3da326817e7c5aab4e30389b42266d35387b569
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-09-15 20:57:33 -0700

    Adds BasicBuilder utility, and static ConfigBuilder

---
+++ src/BetterConsoleTables/Common/BasicBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables.Common
+{
+    public interface IBasicBuilder<T> where T : new()
+    {
+        IBasicBuilder<T> With(Action<T> action);
+        T Build();
+    }
+
+    public class BasicBuilder<T> : IBasicBuilder<T> where T : new()
+    {
+        List<Action<T>> _actions;
+
+        public BasicBuilder()
+        {
+            _actions = new List<Action<T>>();
+        }
+
+        public IBasicBuilder<T> With(Action<T> action)
+        {
+            _actions.Add(action);
+            return this;
+        }
+
+        public T Build()
+        {
+            var config = new T();
+            foreach (var action in _actions)
+            {
+                action.Invoke(config);
+            }
+
+            return config;
+        }
+    }
+}
---
+++ src/BetterConsoleTables/Configuration/ConfigBuilder.cs
@@ -0,0 +1,17 @@
+﻿using BetterConsoleTables.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables.Configuration
+{
+    public static class ConfigBuilder
+    {
+        public static IBasicBuilder<TableCellConfig> CellConfig => new BasicBuilder<TableCellConfig>();
+        public static IBasicBuilder<TableConfiguration> TableConfig => new BasicBuilder<TableConfiguration>();
+    }
+
+
+}
