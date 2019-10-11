7375f4bfc13fd3c9b446f09611a8745fc57b2fcf
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-10-11 15:57:06 -0700

    Adds FormatType enums

--- src/BetterConsoleTables/Common/Enums.cs
+++ src/BetterConsoleTables/Common/Enums.cs
@@ -18,4 +18,37 @@ namespace BetterConsoleTables
         Foreground = 38,
         Background = 48
     }
+
+    [Flags]
+    public enum FormatType
+    {
+        Bold = 1,
+        Italic = 2,
+        Underline = 4,
+        Overline = 8,
+        CrossedOut = 16,
+        Blink = 32
+    }
+
+    [Flags]
+    internal enum AllFormatType
+    {
+        Bold = 1,
+        Italic = 2,
+        Underline = 4,
+        Overline = 8,
+        CrossedOut = 16,
+        Blink = 32,
+        ForegroundColor = 64,
+        BackgroundColor = 128
+    }
+
+    [Flags]
+    internal enum ColorFormatType
+    {
+        Foreground = 64,
+        Background = 128
+    }
+
+
 }
