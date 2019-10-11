81bffe1ff05b62fe75e5b6722e7fa7d99c96aafe
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-10-11 16:01:00 -0700

    Adds a Gradient class

---
+++ src/BetterConsoleTables/Models/Gradient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsoleTables.Models
+{
+    public class Gradient
+    {
+        public Gradient(Color start, Color end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        Color Start { get; set; }
+        Color End { get; set; }
+    }
+}
