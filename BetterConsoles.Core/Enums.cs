56185d62e475813d0f57faf8433011aaf8cd00c2
Douglas <1400380+douglasg14b@users.noreply.github.com>
2019-10-12 16:55:52 -0700

    Adds BetterConsole.Core
    Moves certain shared bits to this project

---
+++ BetterConsoles.Core/Enums.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterConsole.Core
+{
+    public enum Alignment
+    {
+        Left = 0,
+        Right = 1,
+        Center = 2
+    }
+
+    public enum ColorPlane
+    {
+        Foreground = 38,
+        Background = 48
+    }
+
+    /// <summary>
+    /// Extended Font Styles
+    /// </summary>
+    [Flags]
+    public enum FontStyleExt
+    {
+        None = 0,
+        Bold = 1,
+        Italic = 2,
+        Underline = 4,
+        Blink = 8,
+        CrossedOut = 16,
+        Overline = 32
+    }
+
+    [Flags]
+    public enum FormatType
+    {
+        None = 0,
+        Bold = 1,
+        Italic = 2,
+        Underline = 4,
+        Blink = 8,
+        CrossedOut = 16,
+        Overline = 32,
+        ForegroundColor = 64,
+        BackgroundColor = 128
+    }
+
+    [Flags]
+    public enum ColorFormatType
+    {
+        None = 0,
+        Foreground = 64,
+        Background = 128
+    }
+}
